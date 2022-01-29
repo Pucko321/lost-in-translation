39d76821aeddeac5329e55c67d698108d743276e
JonasBM <jonas.mela@gmail.com>
2022-01-29 20:02:53 +0100

    FEAT: login skeleton

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -4,9 +4,24 @@
  */
 const Login = () => {
     return (
-        <main className="Login">
-            <h1>Login</h1>
-        </main>
+        <>
+            <section className="Welcome-message">
+                <h1>Lost In Translation</h1>
+                <img src={"./assets/logo/logo-hello.png"} alt="hello-logo" />
+            </section>
+            <main className="Login">
+                <form>
+                    <h2>Login</h2>
+
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input id="username" type="text" placeholder="What's your name?" />
+                    </div>
+
+                    <button type="submit" className="Login">Login</button>
+                </form>
+            </main>
+        </>
     )
 }
 
