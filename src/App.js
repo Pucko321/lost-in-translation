a670a0343eb3dece7f3bdff81ed73151f66b7c61
JonasBM <jonas.mela@gmail.com>
2022-01-29 16:24:27 +0100

    FEAT: add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './styles/App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Redirect
+} from 'react-router-dom'
+import Login from './views/Login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <h1>Home</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+            <h1>Home</h1>
+        </header>
+        <Routes>
+          <Route path="/" exact element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/views/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <main className="login">
+            <h1>Login</h1>
+        </main>
+    )
+}
+
+export default Login
