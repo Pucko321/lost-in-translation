0050bad1d2bcba9367e2e26542a4f177e11f9e4f
JonasBM <jonas.mela@gmail.com>
2022-01-29 21:03:04 +0100

    FEAT: hoc for profile

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -2,6 +2,13 @@
  * Dependencies
  * @ignore
  */
+ import HeadAppContainer from "../hoc/HeadAppContainer"
+ import NormalAppContainer from "../hoc/NormalAppContainer"
+
+/**
+ * Dependencies
+ * @ignore
+ */
 import { useNavigate } from "react-router-dom"
 
 /**
@@ -13,22 +20,25 @@ import { useNavigate } from "react-router-dom"
 
     return (
         <main className="Profile">
-            <section className="User-info">
-                <h1 onClick={() => navigate('/')}>User Info</h1>
-                <div className="Profile-info-container">
-                    <p>Username:</p>
-                    <p>Foo Bar</p>
-                </div>
-            </section>
+            <HeadAppContainer>
+                <section className="User-info">
+                    <h1 onClick={() => navigate('/')}>User Info</h1>
+                    <div className="Profile-info-container">
+                        <p>Username:</p>
+                        <p>Foo Bar</p>
+                    </div>
+                </section>
+            </HeadAppContainer>
             
-            <section className="User-translations">
-                <h1>Translations</h1>
-                <div className="User-translation-container">
-                    <p>Translation:</p>
-                    <p>Foo Bar</p>
-                </div>
-                
-            </section>
+            <NormalAppContainer>
+                <section className="User-translations">
+                    <h1>Translations</h1>
+                    <div className="User-translation-container">
+                        <p>Translation:</p>
+                        <p>Foo Bar</p>
+                    </div>
+                </section>
+            </NormalAppContainer>
         </main>
     )
 }
