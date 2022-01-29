dd1e0f87c6cff7c0420bd1ae64cdddc58e6c1c60
JonasBM <jonas.mela@gmail.com>
2022-01-29 20:21:58 +0100

    FEAT: translation skeleton

--- src/views/Translation.jsx
+++ src/views/Translation.jsx
@@ -5,7 +5,21 @@
  const Translation = () => {
     return (
         <main className="Translation">
-            <h1>Translation</h1>
+            <form>
+                <div>
+                    <label htmlFor="username">Translation</label>
+                    <input id="username" type="text" placeholder="What do you wish to translate?" />
+                </div>
+
+                <button type="submit" className="Login">Translate</button>
+            </form>
+
+            <section className="Translated-message">
+                <h2>Translation</h2>
+                <div id="Translated-message">
+                    Images...
+                </div>
+            </section>
         </main>
     )
 }
