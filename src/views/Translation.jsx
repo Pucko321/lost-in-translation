58315a0e6bab21e30ee1eed0d21e72607f07cdb4
JonasBM <jonas.mela@gmail.com>
2022-01-29 21:05:08 +0100

    FEAT: hoc for translation

--- src/views/Translation.jsx
+++ src/views/Translation.jsx
@@ -1,25 +1,36 @@
 /**
+ * Dependencies
+ * @ignore
+ */
+ import HeadAppContainer from "../hoc/HeadAppContainer"
+ import NormalAppContainer from "../hoc/NormalAppContainer"
+
+/**
  * Component
  * @ignore
  */
  const Translation = () => {
     return (
         <main className="Translation">
-            <form>
-                <div>
-                    <label htmlFor="username">Translation</label>
-                    <input id="username" type="text" placeholder="What do you wish to translate?" />
-                </div>
+            <HeadAppContainer>
+                <form>
+                    <div>
+                        <label htmlFor="username">Translation</label>
+                        <input id="username" type="text" placeholder="What do you wish to translate?" />
+                    </div>
 
-                <button type="submit" className="Login">Translate</button>
-            </form>
+                    <button type="submit" className="Login">Translate</button>
+                </form>
+            </HeadAppContainer>
 
-            <section className="Translated-message">
-                <h2>Translation</h2>
-                <div id="Translated-message">
-                    Images...
-                </div>
-            </section>
+            <NormalAppContainer>
+                <section className="Translated-message">
+                    <h2>Translation</h2>
+                    <div id="Translated-message">
+                        Images...
+                    </div>
+                </section>
+            </NormalAppContainer>
         </main>
     )
 }
