0b3d5023da8edcdbf976bf6c77368cabbd8136f8
JonasBM <jonas.mela@gmail.com>
2022-01-29 21:01:05 +0100

    STYLE: hoc for login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,26 +1,37 @@
 /**
+ * Dependencies
+ * @ignore
+ */
+import HeadAppContainer from "../hoc/HeadAppContainer"
+import NormalAppContainer from "../hoc/NormalAppContainer"
+
+/**
  * Component
  * @ignore
  */
 const Login = () => {
     return (
         <>
-            <section className="Welcome-message">
-                <h1>Lost In Translation</h1>
-                <img src={"./assets/logo/logo-hello.png"} alt="hello-logo" />
-            </section>
-            <main className="Login">
-                <form>
-                    <h2>Login</h2>
+            <HeadAppContainer>
+                <section className="Welcome-message">
+                    <h1>Lost In Translation</h1>
+                    <img src={"./assets/logo/logo-hello.png"} alt="hello-logo" />
+                </section>
+            </HeadAppContainer>
+            <NormalAppContainer>
+                <main className="Login">
+                    <form>
+                        <h2>Login</h2>
 
-                    <div>
-                        <label htmlFor="username">Username</label>
-                        <input id="username" type="text" placeholder="What's your name?" />
-                    </div>
+                        <div>
+                            <label htmlFor="username">Username</label>
+                            <input id="username" type="text" placeholder="What's your name?" />
+                        </div>
 
-                    <button type="submit" className="Login">Login</button>
-                </form>
-            </main>
+                        <button type="submit" className="Login">Login</button>
+                    </form>
+                </main>
+            </NormalAppContainer>
         </>
     )
 }
