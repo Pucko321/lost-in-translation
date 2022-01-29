5f915668187db23d849266d80419420fb9dd5840
JonasBM <jonas.mela@gmail.com>
2022-01-29 20:45:52 +0100

    CHORE(hoc): add head & normal hoc

---
+++ src/hoc/HeadAppContainer.jsx
@@ -0,0 +1,15 @@
+/**
+ * hoc
+ * @ignore
+ */
+const HeadAppContainer = ({ children }) => {
+    return (
+        <div className="Head-app-container">{ children }</div>
+    )
+}
+
+/**
+ * Export
+ * @ignore
+ */
+export default HeadAppContainer
