bcca281212bc1e225cd35f9646d05cc5a9804b5e
JonasBM <jonas.mela@gmail.com>
2022-01-31 09:42:39 +0100

    REFACTURE: classNames

--- src/components/Translation/TranslatedMessage.jsx
+++ src/components/Translation/TranslatedMessage.jsx
@@ -6,26 +6,13 @@
  const TranslatedMessage = () => {
     return (
         <section id="Translated-message-box">
-            <div id="Translation-message-header">
+            <div id="Translated-message-header">
                 <h2>Translation</h2>
             </div>
             <div id="Translated-message">
                 <img src={"./assets/signs/a.png"} alt="jk" />
+                <p className="Translated-message-separation">-</p>
                 <img src={"./assets/signs/a.png"} alt="jk" />
-                <img src={"./assets/signs/a.png"} alt="jk" />
-                <img src={"./assets/signs/a.png"} alt="jk" />
-                <img src={"./assets/signs/a.png"} alt="jk" />
-                <img src={"./assets/signs/a.png"} alt="jk" />
-                <p className="Translation-separation">-</p>
-                <img src={"./assets/signs/a.png"} alt="jk" />
-                <img src={"./assets/signs/a.png"} alt="jk" />
-                <img src={"./assets/signs/a.png"} alt="jk" />
-                <img src={"./assets/signs/a.png"} alt="jk" />
-                <img src={"./assets/signs/a.png"} alt="jk" />
-                <p className="Translation-separation">-</p>
-                <img src={"./assets/signs/a.png"} alt="jk" />
-                <img src={"./assets/signs/a.png"} alt="jk" />
-                
             </div>
         </section>
     )
