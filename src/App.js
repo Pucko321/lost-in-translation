3a2a234edfcd175c8e8a8613bf68e619f869bf72
Jørgen Saanum <jorgen.saanum@no.experis.com>
2022-02-01 19:03:07 +0100

    REFACTOR: remove unused basename from HashRouter

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Header from "./components/Header"
 
 function App() {
   return (
-    <HashRouter basename={basename}>
+    <HashRouter>
       <div className="App">
         <Header />
         <Routes>
