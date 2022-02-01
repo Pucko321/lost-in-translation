2709fe1a521a9470e5addc380d3509195034a7b8
Jørgen Saanum <jorgen.saanum@no.experis.com>
2022-02-01 20:38:03 +0100

    DOCS(storage.js): added JSDoc to exported functions

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,7 +1,17 @@
+/**
+ * Save JSON object to localStorage as string
+ * @param {string} key The key in the localStorage object
+ * @param {object} value The JSON item to save to localStorage 
+ */
 export const storageSave = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value))
 }
 
+/**
+ * Read JSON string in localStorage and return it as a JavaScript object
+ * @param {string} key The key in the localStorage object
+ * @returns {object} Object read from localstorage
+ */
 export const storageRead = key => {
     const data = localStorage.getItem(key)
     if (data) {
@@ -11,6 +21,10 @@ export const storageRead = key => {
     return null
 }
 
+/**
+ * Delete an item from localStorage using a key
+ * @param {string} key The key in the localStorage object
+ */
 export const storageDelete = key => {
     localStorage.removeItem(key)
 }
