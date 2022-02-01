d2e5cb76e53a39f525b197a3389cbad0703ac83b
JonasBM <jonas.mela@gmail.com>
2022-02-01 12:57:15 +0100

    REFACTOR: header into own component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
 import './styles/App.css'
-import {Routes, Route, NavLink, HashRouter } from 'react-router-dom'
+import {Routes, Route, HashRouter } from 'react-router-dom'
 import Login from './views/Login.jsx'
 import NotFound from './views/NotFound.jsx'
 import Translation from './views/Translation.jsx'
 import Profile from './views/Profile.jsx'
-import { STORAGE_KEY_APP_NAME } from './const/storageKeys'
-import { useUser } from "./context/UserContext"
+import Header from "./components/Header"
 
 function App() {
   const basename =
   process.env.NODE_ENV === "production"
   ? '/'
   : '/'
-  const { user } = useUser()
 
   return (
     <HashRouter basename={basename}>
       <div className="App">
-        <header className="App-header">
-            {user !== null && <img src="./assets/logo/logo.png" alt="logo" />}
-            <h1>{ STORAGE_KEY_APP_NAME }</h1>
-            {user !== null &&
-            <nav>
-              <li>
-                <NavLink className="App-link" to="/translate">Translate</NavLink>
-              </li>
-              <li>
-                <NavLink className="App-link" to="/profile">Profile</NavLink>
-              </li>
-            </nav>}
-        </header>
+        <Header />
         <Routes>
           <Route path="/" element={< Login />} />
           <Route path="/translate" exact element={< Translation />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { STORAGE_KEY_APP_NAME } from '../const/storageKeys'
+import { NavLink } from 'react-router-dom'
+import { useUser } from "../context/UserContext"
+
+/**
+ * Component
+ * @ignore
+ */
+ const Header = () => {
+    const { user } = useUser()
+
+    return (
+        <header className="App-header">
+            {user !== null && <img src="./assets/logo/logo.png" alt="logo" />}
+            <h1>{ STORAGE_KEY_APP_NAME }</h1>
+            {user !== null &&
+            <nav>
+              <li>
+                <NavLink className="App-link" to="/translate">Translate</NavLink>
+              </li>
+              <li>
+                <NavLink className="App-link" to="/profile">Profile</NavLink>
+              </li>
+            </nav>}
+        </header>
+    )
+}
+
+/**
+ * Export
+ * @ignore
+ */
+export default Header
