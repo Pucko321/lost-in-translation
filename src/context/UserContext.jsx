4838c57489fd8e08bb7fdd38ab17eaa10040af8a
Jørgen Saanum <jorgen.saanum@no.experis.com>
2022-02-01 19:05:14 +0100

    DOCS: clarify documentation on user checking

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -32,7 +32,7 @@ const UserProvider = ({ children }) => {
         if (!user)
             return
 
-        // Clear localStorage and user if it does not exist in the API
+        // Clear localStorage and user if the user does not exist in the API
         checkForUser(storageRead(STORAGE_KEY_USER)?.username).then(([error, user]) => {
             if (error || user.length === 0) {
                 localStorage.clear()
